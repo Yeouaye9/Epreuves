24450f57c0ac305c604bd70c82abea189462f9e7
Yeouaye9 <104650392+Yeouaye9@users.noreply.github.com>
2022-04-29 20:14:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Epreuves du monde</title>
+</head>
+<body style="background: #0678b7;">
+<?php include("pap.php"); ?>
+	<div style="float: right;">
+		<img src="Images/image1.png">
+	</div>
+	<div style="width: 100%; height: 800px;"></div>
+</body>
+		<?php include("footer.php"); ?>
+</html>
